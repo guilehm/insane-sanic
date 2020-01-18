deae2abb7c4146d6964a7332d201dd78e499949a
guilherme <guile.hm@hotmail.com>
2020-01-18 19:28:30 -0300

    create structure

---
+++ main.py
@@ -0,0 +1,14 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic(__name__)
+app.static('/', 'hello-world.html')
+
+
+@app.websocket('/socket')
+async def socket_route(request, ws):
+    return json({"hello": "world"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
